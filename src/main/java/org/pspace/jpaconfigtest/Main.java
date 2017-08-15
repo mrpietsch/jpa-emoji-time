e491fdceea0c79894ab4d4ca434d2057c82bd7e3
Martin Pietsch <martin.pietsch@volkswagen.de>
2017-08-15 22:28:56 +0200

    Setup a simple Spring Boot app

---
+++ src/main/java/org/pspace/jpaconfigtest/Main.java
@@ -0,0 +1,15 @@
+package org.pspace.jpaconfigtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
